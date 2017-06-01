020181b9d32b4ba5d19c1d9cfe0b92a02e7d8b12
Iris <mdza.iris@gmail.com>
2017-06-01 13:56:46 -0700

    route speaker and sessions

--- js/navigation/router.js
+++ js/navigation/router.js
@@ -3,12 +3,16 @@ import NavigationLayout from '../navigation/NavigationLayout';
 import About from '../scenes/About';
 import Schedule from '../scenes/Schedule';
 import Faves from '../scenes/Faves';
+import Session from '../scenes/Session';
+import Speaker from '../scenes/Speaker';
 
 const Router = createRouter(() => ({
   navigation: () => NavigationLayout,
   about: () => About,
   schedule: () => Schedule,
-  faves: () => Faves
+  faves: () => Faves,
+  session: () => Session,
+  speaker: () => Speaker
 
 }));
 
